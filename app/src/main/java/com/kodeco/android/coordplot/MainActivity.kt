d197c328a29dccd5e7f751d324cb961f2308f07d
Heidi Proske <heidiproske@users.noreply.github.com>
2024-01-29 11:38:16 -0800

    Add blue box size 300

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -3,12 +3,15 @@ package com.kodeco.android.coordplot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,14 +25,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Builds out the plot surface
 @Composable
 fun PlotSurface() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = Color.White
     ) {
-        // TODO Build out the plot surface
-        //  This should include a Column composable that
+        Map(xPercent = 0.5f, yPercent = 0.5f)
+        //  TODO: This should include a Column composable that
         //  includes a Map, and two MapSlider composables
         //  (one slider for each axis).
     }
@@ -47,9 +51,13 @@ fun PlotSurfacePreview() {
 @Composable
 fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
     // TODO fill out the square map here.
-    //  Create a Box composable with a size of 300.dp
     //  and then create an inner Box composable
     //  with a clip shape of CircleShape.
+    Box(
+        modifier = modifier
+            .size(300.dp)
+            .background(Color.Blue)
+    )
 }
 
 @Preview(showBackground = true)
