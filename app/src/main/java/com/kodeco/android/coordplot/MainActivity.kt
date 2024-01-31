825758c0eed33ea5239d026a101b0f4fc7750ec2
Heidi Proske <heidiproske@users.noreply.github.com>
2024-01-31 14:16:36 -0800

    Fix bug - Use dynamic box size
    Previously, if someone would change the PLOT_SIZE constant to something like 600dp that wouldn't fit on a smaller device - we wouldn't get our guaranteed square plot on the screen.
    
    So now i'm storing the "current box size" (after it's aspect ratio and preferred size have been set) so that I can take into account any adjustments that need to be made in order to plot the dot!

--- app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
+++ app/src/main/java/com/kodeco/android/coordplot/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.kodeco.android.coordplot
 
 import android.os.Bundle
+import android.util.Size
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -18,12 +20,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kodeco.android.coordplot.ui.theme.MyApplicationTheme
@@ -81,17 +88,30 @@ fun PlotSurfacePreview() {
 
 @Composable
 fun Map(xPercent: Float, yPercent: Float, modifier: Modifier = Modifier) {
+    val density = LocalDensity.current.density
+    var boxSize by remember { mutableStateOf(Size(0, 0)) }
+
     Box(
         modifier = modifier
             .size(PLOT_SIZE.dp)
+            .aspectRatio(1f) // Ensure the plot is a square (1:1 aspect ratio)
             .background(PLOT_BACKGROUND_COLOR)
+            .onGloballyPositioned { coordinates ->
+                boxSize = Size(
+                    ((coordinates.size.width / density).toInt()),
+                    ((coordinates.size.height / density).toInt())
+                )
+            }
     ) {
         val dotRadius = DOT_DIAMETER / 2
+        val offsetX = (xPercent * boxSize.width)
+        val offsetY = (yPercent * boxSize.height)
+
         Box(
             modifier = modifier
                 .offset(
-                    x = (xPercent * PLOT_SIZE - dotRadius).dp,
-                    y = (yPercent * PLOT_SIZE - dotRadius).dp
+                    x = offsetX.dp - dotRadius.dp,
+                    y = offsetY.dp - dotRadius.dp
                 )
                 .size(DOT_DIAMETER.dp)
                 .clip(CircleShape)
